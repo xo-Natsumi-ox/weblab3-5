42d95feb35c1c09520bdd80aa7b3c73916f0f7a0
xo-Natsumi-ox <53785210+xo-Natsumi-ox@users.noreply.github.com>
2020-10-26 03:42:14 +0200

    Add files via upload

--- js/movie.js
+++ js/movie.js
@@ -6,31 +6,36 @@ let film = [{
         id: 1,
         name: "The Shawshank Redemption",
         rate: 9.2,
-        duration: 142
+        duration: 142,
+        img: "image/shawshank.jpg"
     },
     {
         id: 2,
         name: "Up",
         rate: 8.2,
-        duration: 96
+        duration: 96,
+        img: "image/up.jpg"
     },
     {
         id: 3,
         name: "Requiem for a Dream",
         rate: 8.3,
-        duration: 102
+        duration: 102,
+        img: "image/Requiem.jpg"
     },
     {
         id: 4,
-        name: "Harry Potter and the Sorcerer's Stone",
+        name: "Harry Potter and the philosopher's stone",
         rate: 7.6,
-        duration: 152
+        duration: 152,
+        img: "image/harry.jpg"
     },
     {
         id: 5,
         name: "Shrek",
         rate: 7.8,
-        duration: 90
+        duration: 90,
+        img: "image/shrek.jpg"
     }
 ]
 let films = film
@@ -61,22 +66,28 @@ function sortedFilm() {
     let sortBy = document.getElementById('sorting').value;
     console.log(sortBy);
     if (sortBy == 'none') {
-        show(films);
+        showFilms(films);
         return;
     } else if (sortBy == 'rate') {
         films.sort(compareByRate);
-    } else if (sortBy == 'duration') {
-        films.sort(compareByDuration);
+    } else if (sortBy == 'short') {
+        films.sort(compareByDurationAsc);
+    } else if (sortBy == 'long') {
+        films.sort(compareByDurationDesc);
     }
-    show(films);
+    showFilms(films);
 }
 
 function compareByRate(first, second) {
-    return first.rate - second.rate;
+    return second.rate - first.rate;
 }
 
-function compareByDuration(first, second) {
+function compareByDurationAsc(first, second) {
     return first.duration - second.duration;
+}
+
+function compareByDurationDesc(first, second) {
+    return second.duration - first.duration;
 }
 
 
@@ -86,6 +97,7 @@ const showFilms = (movies) => {
     const showMovies = movies.map((film) => {
         return `
         <li class="movie">
+            <img src="${film.img}">
             <h1> ${film.name}</h1>
             <h3>Rating on IMDb: ${film.rate}</h3>
             <h3>Duration: ${film.duration} minutes</h3>
